3824865c2f03c91c87b0110f61daed2c35a110e5
nathanmueller007 <nathanmueller@student.lccc.wy.edu>
2025-11-14 22:55:10 +0000

    assigned variables

---
+++ Main_code.py
@@ -0,0 +1,19 @@
+# Here is all of the code for the final project
+
+# my teams shooting percentages
+three_point_percentage = 0.35
+
+two_point_percentage = 0.50
+
+# Opponents free throw percentage
+
+opponent_free_throw_percent = 0.60
+
+# time remaining
+time_remaining = 30
+
+offensive_rebound_prob = 0.25
+
+overtime_win_prob = 0.50
+
+num_trials = 1000
