eb6f13683826510491f8b4de8d591db2804038b1
nathanmueller007 <nathanmueller@student.lccc.wy.edu>
2025-12-06 01:30:12 +0000

    added basic 3- point scenario not stress tested

--- Main_code.py
+++ Main_code.py
@@ -1,11 +1,13 @@
-# Here is all of the code for the final project
+   # Here is all of the code for the final project
+
+import random
 
 # my teams shooting percentages
 three_point_percentage = 0.35
 
 two_point_percentage = 0.50
 
-# Opponents free throw percentage
+    # Opponents free throw percentage
 
 opponent_free_throw_percent = 0.60
 
@@ -17,3 +19,31 @@ offensive_rebound_prob = 0.25
 overtime_win_prob = 0.50
 
 num_trials = 1000
+
+def simulate_endgame(trials=num_trials):
+    wins_three, points_three = 0, 0
+    wins_foul, points_foul = 0, 0
+
+    for _ in range(trials):
+
+        # Strategy A: Take 3- pointer
+        if random.random() < three_point_percentage:
+            wins_three += 1
+            points_three += 3
+        else: 
+            # Missed shot chance for offensive rebound
+            if random.random() < offensive_rebound_prob:
+                if random.random() < two_point_percentage:
+                    #Tie game - goes into overtime
+
+                    if random.random() < overtime_win_prob:
+                        wins_three += 1
+                    points_three += 2
+                else:
+                    points_three += 0
+            else: 
+                points_three += 0
+
+
+
+
